412a18af048ff0cdad305c6cc95b4aa81838c816
tfu2003 <officialtonyfu@gmail.com>
2023-02-02 12:20:05 -0800

    formatted intro layout

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Profile from './images/profile.jpeg'
+import React from "react";
+import Profile from "./images/profile.jpeg";
 
 function Intro() {
   return (
@@ -7,13 +7,20 @@ function Intro() {
       <h1 className="text-3xl">
         Hello there! My name is Tony and welcome to my website!
       </h1>
-      <img src={Profile} alt="profile" className="max-w-xs pt-6"/> 
-      <h1> About me</h1>
-      <div> 
-        <p>Something about me here idk. While I'm not debugging code, you can either find me at the gym curling dumbbells, binge-watching an entire Netflix series in 1 day, or fulfilling my ever increasing need for caffeine</p> 
+      <div className="flex justify-between p-6">
+        <img src={Profile} alt="profile" className="max-w-xs"/>
+        <div className="flex flex-col items-center">
+          <h1 className="text-3xl"> About me!</h1>
+          <p className="text-lg p-6">
+            Something about me here idk. While I'm not debugging code, you can
+            either find me at the gym curling dumbbells, binge-watching an
+            entire Netflix series in 1 day, or fulfilling my ever increasing
+            need for caffeine.
+          </p>
+        </div>
       </div>
     </div>
   );
 }
 
-export default Intro
+export default Intro;
