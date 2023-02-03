a62f698b7ecbe3024cb68dc47764558e8a53d4d5
tfu2003 <officialtonyfu@gmail.com>
2023-02-02 23:02:48 -0800

    added background color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Intro from './components/intro'
-import Header from './components/header'
+import Intro from "./components/intro";
+import Header from "./components/header";
+import Background from "./components/images/background.jpeg";
 
 function App() {
   return (
-    <div> 
-    <div> <Header/> </div>
-    <div> <Intro/> </div>
+    <div className="bg-gradient-to-r from-violet-200 to-pink-200">
+      <div>
+      <Header />
+      <Intro />
+      </div>
     </div>
   );
 }
--- src/components/intro.js
+++ src/components/intro.js
@@ -7,15 +7,20 @@ function Intro() {
       <h1 className="text-3xl">
         Hello there! My name is Tony and welcome to my website!
       </h1>
-      <div className="flex justify-between p-6">
-        <img src={Profile} alt="profile" className="max-w-xs"/>
+      <div className="flex justify-between p-6 pt-24 w-2/3 space-x-10">
+        <img src={Profile} alt="profile" className="max-w-xs" />
         <div className="flex flex-col items-center">
-          <h1 className="text-3xl"> About me!</h1>
+          <h1 className="text-3xl pt-6"> About me!</h1>
           <p className="text-lg p-6">
-            Something about me here idk. While I'm not debugging code, you can
-            either find me at the gym curling dumbbells, binge-watching an
-            entire Netflix series in 1 day, or fulfilling my ever increasing
-            need for caffeine.
+            I am a self-motivated and passionate individual seeking to expand my
+            knowledge in technology. Currently, I am a second year Computer
+            Science student at the University of British Columbia. At the
+            moment, I am interested in web development and machine learning.
+            <br />
+            <br />
+            While I'm not debugging code, you can either find me at the gym
+            curling dumbbells, binge-watching an entire Netflix series in 1 day,
+            or fulfilling my ever increasing need for caffeine.
           </p>
         </div>
       </div>
