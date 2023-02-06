41736699537b401cc9d05e0d230020a1d5056080
tfu2003 <officialtonyfu@gmail.com>
2023-02-06 15:40:24 -0800

    added button for intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,4 +1,4 @@
-import { React} from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import Characters from "./characters";
 import FadeIn from "react-fade-in";
@@ -35,26 +35,31 @@ function Intro() {
         </div>
       </motion.div>
       <div className="flex justify-between p-6 pt-24 w-2/3 space-x-10">
-        <FadeIn delay="2000" transitionDuration="1000"> 
-        <img src={Profile} alt="profile" className="max-w-xs" />
+        <FadeIn delay="2000" transitionDuration="1000">
+          <img src={Profile} alt="profile" className="max-w-xs" />
         </FadeIn>
         <div className="flex flex-col items-center">
           <FadeIn delay="2200" transitionDuration="1000">
-          <h1 className="text-3xl pt-6"> About me!</h1>
+            <h1 className="text-3xl pt-6"> About me!</h1>
           </FadeIn>
           <FadeIn delay="2200" transitionDuration="1000">
-          <p className="text-lg p-6">
-            I am a self-motivated and passionate individual seeking to expand my
-            knowledge in technology. Currently, I am a second year Computer
-            Science student at the University of British Columbia. At the
-            moment, I am interested in web development and machine learning.
-            <br />
-            <br />
-            While I'm not debugging code, you can either find me at the gym
-            curling dumbbells, binge-watching an entire Netflix series in 1 day,
-            or fulfilling my ever increasing need for caffeine.
-          </p>
-          </FadeIn> 
+            <p className="text-lg p-6">
+              I am a self-motivated and passionate individual seeking to expand
+              my knowledge in technology. Currently, I am a second year Computer
+              Science student at the University of British Columbia. At the
+              moment, I am interested in web development and machine learning.
+              <br />
+              <br />
+              While I'm not debugging code, you can either find me at the gym
+              curling dumbbells, binge-watching an entire Netflix series in 1
+              day, or fulfilling my ever increasing need for caffeine.
+            </p>
+          </FadeIn>
+          <FadeIn delay="2400" transitionDuration="1000">
+            <button class="border-2 border-pink-500 rounded-full text-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-4">
+              See more!
+            </button>
+          </FadeIn>
         </div>
       </div>
     </div>
