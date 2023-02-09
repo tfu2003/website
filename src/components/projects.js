5b49523f0fdd9c9d9696c2ae6a2a828d4bb7f862
tfu2003 <officialtonyfu@gmail.com>
2023-02-09 02:08:27 -0800

    formatted reci one in projects component

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import Speech from "./images/speech-simulator.png";
-import Reci from "./images/reci-one.png"
+import Reci from "./images/reci-one.png";
 
 function Projects() {
   return (
@@ -33,11 +33,9 @@ function Projects() {
               </a>
             </p>
           </div>
-          <div className="pt-6 flex flex-col items-center">
-            <div className="text-lg font-bold">
-              Tools used:
-            </div>
-            <div className="flex items-end text-sm pt-4 space-x-5">
+          <div className="pt-6">
+            <div className="text-center text-lg font-bold">Tools used:</div>
+            <div className="grid grid-cols-4 gap-4 text-sm pt-4">
               <a
                 href="https://nextjs.org/docs"
                 target="_blank"
@@ -73,52 +71,41 @@ function Projects() {
             </div>
           </div>
           <div className="pt-4 text-end">
-                <a
-                  href="https://github.com/marcusgchan/speech-simulator"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="font-bold"
-                >
-                  Link to Github
-                </a>
-            </div>
+            <a
+              href="https://github.com/marcusgchan/speech-simulator"
+              target="_blank"
+              rel="noreferrer"
+              className="font-bold"
+            >
+              Link to Github
+            </a>
+          </div>
         </div>
       </div>
+
       <div className="flex p-6 pt-20 w-5/6 -space-x-12">
         <div className="flex flex-col items-start w-2/5">
-          <h1 className="text-xl pt-6 font-bold text-start">
-            Reci-one
-          </h1>
-          <div className="pt-6">
+          <h1 className="text-xl pt-6 font-bold text-end">Reci One</h1>
+          <div className="pt-6 z-10">
             <p className="text-base bg-pink-300 p-5">
-              VR Speech Simulator allows you practice your public speaking
-              within an immersive environment. Import a script and put on a
-              headset to get started. Once started, VR Speech Simulator will
-              provide feedback on how to perfect your speech!
+              Need a way to keep a record of your recipes? Reci One is a recipe
+              website that lets users add, view and delete recipes. Users can
+              filter recipes based on key words such as ingredients and
+              nationalities and also set recipes as public or private. Keep your
+              recipes safe with Reci One!
               <br />
               <br />
-              Placed 3rd out of 131 projects in the largest hackathon in Western
-              Canada. Interested?&nbsp;
-              <a
-                href="https://devpost.com/software/vr-speech-simulator"
-                target="_blank"
-                rel="noreferrer"
-                className="font-bold"
-              >
-                Click to read more!
-              </a>
+              More functionality to come!
             </p>
           </div>
-          <div className="pt-6 flex flex-col items-center">
-            <div className="text-lg font-bold">
-              Tools used:
-            </div>
-            <div className="flex items-end text-sm pt-4 space-x-5">
+          <div className="pt-6">
+            <div className="text-center text-lg font-bold">Tools used:</div>
+            <div className="grid grid-cols-4 gap-4 text-sm pt-4">
               <a
                 href="https://nextjs.org/docs"
                 target="_blank"
                 rel="noreferrer"
-                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
               >
                 Next.js
               </a>
@@ -126,7 +113,7 @@ function Projects() {
                 href="https://www.typescriptlang.org/"
                 target="_blank"
                 rel="noreferrer"
-                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
               >
                 TypeScript
               </a>
@@ -134,32 +121,52 @@ function Projects() {
                 href="https://www.prisma.io/docs"
                 target="_blank"
                 rel="noreferrer"
-                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
               >
                 Prisma
               </a>
               <a
-                href="https://docs.pmnd.rs/react-three-fiber/"
+                href="https://trpc.io/docs"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                tRPC
+              </a>
+            </div>
+            <div className="flex items-center text-sm pt-4 space-x-5">
+              <a
+                href="https://zod.dev/"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                Zod
+              </a>
+              <a
+                href="https://next-auth.js.org/getting-started/introduction"
                 target="_blank"
                 rel="noreferrer"
                 className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450"
               >
-                react-three-fiber
+                NextAuth.js
               </a>
             </div>
           </div>
           <div className="pt-4 text-end">
-                <a
-                  href="https://github.com/marcusgchan/speech-simulator"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="font-bold"
-                >
-                  Link to Github
-                </a>
-            </div>
+            <a
+              href="https://github.com/marcusgchan/speech-simulator"
+              target="_blank"
+              rel="noreferrer"
+              className="font-bold"
+            >
+              Link to Github
+            </a>
+          </div>
         </div>
-      <img src={Reci} alt="speech" className="max-w-2xl" />
+        <div className="z-0">
+          <img src={Reci} alt="speech" className="max-w-2xl" />
+        </div>
       </div>
     </div>
   );
