9043b2434cbc349bca2c6c22700fa989527c4c05
tfu2003 <officialtonyfu@gmail.com>
2023-02-09 21:25:26 -0800

    added trade designer to projects component

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,14 +1,15 @@
 import Speech from "./images/speech-simulator.png";
 import Reci from "./images/reci-one.png";
+import Trade from "./images/trade-designer.png"
 
 function Projects() {
   return (
     <div className="flex min-h-screen flex-col items-center pt-10 font-serif">
       <h1 className="text-3xl font-bold">Projects!</h1>
-      <div className="flex items-center pl-6 pt-16 -space-x-12 w-5/6">
+      <div className="flex items-center pl-6 pt-12 -space-x-12 w-5/6">
         <img src={Speech} alt="speech" className="max-w-2xl" />
         <div className="flex flex-col">
-            <h1 className="text-xl pt-4 font-bold text-end">
+            <h1 className="text-xl font-bold text-end">
               VR Speech Simulator
               <br />
               [nwHacks 2023 Winner]
@@ -81,7 +82,7 @@ function Projects() {
               </a>
             </div>
           </div>
-          <div className="pt-3 text-end">
+          <div className="pt-6 text-end">
             <a
               href="https://github.com/marcusgchan/speech-simulator"
               target="_blank"
@@ -96,7 +97,7 @@ function Projects() {
 
       <div className="flex items-center pl-6 pt-20 w-5/6 -space-x-12">
         <div className="flex flex-col">
-          <h1 className="text-xl pt-6 font-bold text-start">Reci One</h1>
+          <h1 className="text-xl font-bold text-start">Reci One</h1>
           <div className="pt-3 z-10">
             <p className="text-base bg-pink-300 p-5">
               Need a way to keep a record of your recipes? Reci One is a recipe
@@ -166,7 +167,7 @@ function Projects() {
                 </a>
               </div>
           </div>
-          <div className="pt-3 text-start">
+          <div className="pt-6 text-start">
             <a
               href="https://github.com/marcusgchan/reci-one"
               target="_blank"
@@ -179,6 +180,74 @@ function Projects() {
         </div>
         <div className="z-0">
           <img src={Reci} alt="speech" className="max-w-2xl" />
+        </div>
+      </div>
+
+      <div className="flex items-center pt-20 pl-6 -space-x-12 w-5/6">
+        <img src={Trade} alt="speech" className="max-w-2xl" />
+        <div className="flex flex-col">
+            <h1 className="text-xl font-bold text-end">
+              Trade Designer
+            </h1>
+            <div className="pt-3">
+              <p className="text-base bg-pink-300 p-5">
+                VR Speech Simulator allows you practice your public speaking
+                within an immersive environment. Import a script and put on a
+                headset to get started. Once started, VR Speech Simulator will
+                provide feedback on how to perfect your speech!
+                <br />
+                <br />
+                Placed 3rd out of 131 projects in the largest hackathon in
+                Western Canada. Interested?&nbsp;
+                <a
+                  href="https://devpost.com/software/vr-speech-simulator"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="font-bold"
+                >
+                  Click to read more!
+                </a>
+              </p>
+            </div>
+          <div className="flex flex-col items-center pt-3 pl-12">
+            <div className="text-lg font-bold">Tools used:</div>
+            <div className="grid grid-cols-3 gap-4 text-sm pt-3">
+              <a
+                href="https://docs.oracle.com/en/java/"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                Java
+              </a>
+              <a
+                href="https://docs.oracle.com/javase/7/docs/api/javax/swing/package-summary.html"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                Java Swing
+              </a>
+              <a
+                href="https://junit.org/junit5/docs/current/user-guide/"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                JUnit
+              </a>
+            </div>
+          </div>
+          <div className="pt-6 text-end">
+            <a
+              href="https://github.com/marcusgchan/speech-simulator"
+              target="_blank"
+              rel="noreferrer"
+              className="font-bold"
+            >
+              Link to Github
+            </a>
+          </div>
         </div>
       </div>
     </div>
