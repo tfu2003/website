c7fc20a334d30dd1d98d42f2f4fbb6440135a338
tfu2003 <officialtonyfu@gmail.com>
2023-02-16 23:35:36 -0800

    added space invaders to projects component

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import List from "./images/to-do-list.png";
+import Space from "./images/space-invaders.png"
 import { useEffect } from "react";
 
 function Projects() {
@@ -71,6 +72,94 @@ function Projects() {
           </div>
         </div>
       </div>
+
+      <div className="flex items-center pl-6 pt-20 w-5/6 -space-x-12">
+        <div className="flex flex-col">
+          <h1 className="text-xl font-bold text-start">Reci One</h1>
+          <div className="pt-3 z-10">
+            <p className="text-base bg-pink-300 p-5">
+              Need a way to keep a record of your recipes? Reci One is a recipe
+              website that lets users add, view and delete recipes. Users can
+              filter recipes based on key words such as ingredients and
+              nationalities and also set recipes as public or private. Keep your
+              recipes safe with Reci One!
+              <br />
+              <br />
+              More functionality to come!
+            </p>
+          </div>
+          <div className="flex flex-col items-center pt-3 pr-12">
+            <div className="text-lg font-bold">Tools used:</div>
+            <div className="grid grid-cols-4 gap-4 text-sm pt-3">
+              <a
+                href="https://nextjs.org/docs"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                Next.js
+              </a>
+              <a
+                href="https://www.typescriptlang.org/"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                TypeScript
+              </a>
+              <a
+                href="https://www.prisma.io/docs"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                Prisma
+              </a>
+              <a
+                href="https://trpc.io/docs"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                tRPC
+              </a>
+            </div>
+            <div className="grid grid-cols-2 gap-4 text-sm pt-3">
+              <a
+                href="https://zod.dev/"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                Zod
+              </a>
+              <a
+                href="https://next-auth.js.org/getting-started/introduction"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450"
+              >
+                NextAuth.js
+              </a>
+            </div>
+          </div>
+          <div className="pt-6 text-start">
+            <a
+              href="https://github.com/marcusgchan/reci-one"
+              target="_blank"
+              rel="noreferrer"
+              className="font-bold"
+            >
+              Link to Github
+            </a>
+          </div>
+        </div>
+        <div className="z-0">
+          <img src={Space} alt="space" className="max-w-2xl" />
+        </div>
+      </div>
+
+      
 
       <div className="pt-16 pb-5">
         <button
