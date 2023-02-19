7d912001eae8784e195ff65b1916f9bf8f5952e7
tfu2003 <officialtonyfu@gmail.com>
2023-02-19 02:12:08 -0800

    added website to projects component

--- src/components/projects.js
+++ src/components/projects.js
@@ -24,6 +24,117 @@ function Projects() {
       <div className="flex items-center pl-6 pt-12 -space-x-12 w-5/6">
         <img src={List} alt="list" className="max-w-2xl" />
         <div className="flex flex-col">
+          <h1 className="text-2xl font-bold text-end">Personal Website</h1>
+          <div className="pt-3">
+            <p className="text-base bg-pink-300 p-5">
+              A website that I made to showcase my projects and experiences.
+              This is a project that I made which helped me further develop my
+              skills in web development. I also used some tools that I was
+              unfamiliar with prior to making this project.
+            </p>
+          </div>
+          <div className="flex flex-col items-center pt-3 pl-12">
+            <div className="text-lg font-bold">Tools used:</div>
+            <div className="grid grid-cols-3 gap-4 text-sm pt-3">
+              <a
+                href="https://reactjs.org/docs/getting-started.html"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                React
+              </a>
+              <a
+                href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                JavaScript
+              </a>
+              <a
+                href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                HTML
+              </a>
+            </div>
+            <div className="grid grid-cols-1 gap-4 text-sm pt-3">
+              <a
+                href="https://v2.tailwindcss.com/docs"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                Tailwind CSS
+              </a>
+            </div>
+          </div>
+          <div className="pt-6 text-end">
+            <a
+              href="https://github.com/tfu2003/website"
+              target="_blank"
+              rel="noreferrer"
+              className="font-bold"
+            >
+              Link to Github
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center pl-6 pt-20 w-5/6 -space-x-12">
+        <div className="flex flex-col">
+          <h1 className="text-xl font-bold text-start">Space Invaders</h1>
+          <div className="pt-3 z-10">
+            <p className="text-base bg-pink-300 p-5">
+              An interactive game where players shoot incoming aliens that are
+              attacking the planet. I made this to give myself an introduction
+              to game design.
+            </p>
+          </div>
+          <div className="flex flex-col items-center pt-3 pr-12">
+            <div className="text-lg font-bold">Tools used:</div>
+            <div className="grid grid-cols-2 gap-4 text-sm pt-3">
+              <a
+                href="https://docs.python.org/3/"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                Python
+              </a>
+              <a
+                href="https://www.pygame.org/docs/"
+                target="_blank"
+                rel="noreferrer"
+                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
+              >
+                Pygame
+              </a>
+            </div>
+          </div>
+          <div className="pt-6 text-start">
+            <a
+              href="https://github.com/tfu2003/space-invaders"
+              target="_blank"
+              rel="noreferrer"
+              className="font-bold"
+            >
+              Link to Github
+            </a>
+          </div>
+        </div>
+        <div className="z-0">
+          <img src={Space} alt="space" className="max-w-2xl" />
+        </div>
+      </div>
+
+      <div className="flex items-center pl-6 pt-20 -space-x-12 w-5/6">
+        <img src={List} alt="list" className="max-w-2xl" />
+        <div className="flex flex-col">
           <h1 className="text-2xl font-bold text-end">To Do List</h1>
           <div className="pt-3">
             <p className="text-base bg-pink-300 p-5">
@@ -72,53 +183,6 @@ function Projects() {
               Link to Github
             </a>
           </div>
-        </div>
-      </div>
-
-      <div className="flex items-center pl-6 pt-20 w-5/6 -space-x-12">
-        <div className="flex flex-col">
-          <h1 className="text-xl font-bold text-start">Space Invaders</h1>
-          <div className="pt-3 z-10">
-            <p className="text-base bg-pink-300 p-5">
-              An interactive game where players shoot incoming aliens that are
-              attacking the planet. I made this to give myself an introduction
-              to game design.
-            </p>
-          </div>
-          <div className="flex flex-col items-center pt-3 pr-12">
-            <div className="text-lg font-bold">Tools used:</div>
-            <div className="grid grid-cols-2 gap-4 text-sm pt-3">
-              <a
-                href="https://docs.python.org/3/"
-                target="_blank"
-                rel="noreferrer"
-                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
-              >
-                Python
-              </a>
-              <a
-                href="https://www.pygame.org/docs/"
-                target="_blank"
-                rel="noreferrer"
-                className="border-2 border-pink-500 shadow hover:shadow-md hover:shadow-pink-500 hover:opacity-50 p-2 transition duration-450 text-center"
-              >
-                Pygame
-              </a>
-              </div>
-          </div>
-          <div className="pt-6 text-start">
-            <a
-              href="https://github.com/tfu2003/space-invaders"
-              target="_blank"
-              rel="noreferrer"
-              className="font-bold"
-            >
-              Link to Github
-            </a>
-          </div>
-        </div>
-        <div className="z-0">
-          <img src={Space} alt="space" className="max-w-2xl" />
         </div>
       </div>
 
